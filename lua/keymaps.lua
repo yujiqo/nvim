1c698def293968e01e8668df6ee2ee212906e194
yujiqo <danilastolyarov97@gmail.com>
2024-06-14 22:46:44 +0500

    changed keymaps a bit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,10 +8,7 @@ map("n", "X", '"_X')
 map("n", "<C-a>", "ggVG")
 map("n", "<leader>=", "ggVG=gg")
 
-map("i", "<C-k>", "<Up>")
-map("i", "<C-j>", "<Down>")
-map("i", "<C-h>", "<Left>")
-map("i", "<C-l>", "<Right>")
+map("n", ";", ":!")
 
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
@@ -27,11 +24,9 @@ map("n", "<leader>nh", ":nohl<CR>", { silent = true })
 
 map("n", "<leader>te", ":tabedit<CR>", { silent = true })
 map("n", "<leader>h", ":tabmove -<CR>", { silent = true })
-map("n", "<leader><Left>", ":tabmove -<CR>", { silent = true })
 map("n", "<leader>l", ":tabmove +<CR>", { silent = true })
-map("n", "<leader><Right>", ":tabmove +<CR>", { silent = true })
-map("n", "<TAB>", ":BufferLineCycleNext<CR>", { silent = true })
-map("n", "<S-TAB>", ":BufferLineCyclePrev<CR>", { silent = true })
+map("n", "<TAB>", ":tabnext<CR>", { silent = true })
+map("n", "<S-TAB>", ":tabprev<CR>", { silent = true })
 
 map("n", "<leader>sg", ":Gitsigns diffthis<CR>", { silent = true })
 
@@ -43,10 +38,20 @@ map("n", "<leader>s<up>", "<C-w>+")
 map("n", "<leader>s<down>", "<C-w>-")
 map("n", "<leader>sm", ":MaximizerToggle<CR>", { silent = true })
 
+if vim.fn.has("nvim") == 1 then
+    map("n", "<BS>", "<C-w>h", { silent = true})
+else
+    map("n", "<C-h>", "<C-w>h", { silent = true})
+end
+map("n", "<C-j>", "<C-w>j", { silent = true})
+map("n", "<C-k>", "<C-w>k", { silent = true})
+map("n", "<C-l>", "<C-w>l", { silent = true})
+
 map("n", "<leader>/", ":lua require('Comment.api').toggle.linewise.current()<CR>", { silent = true })
 map("v", "<leader>/", ":lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { silent = true })
 
-map("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { silent = true })
+map("n", "<leader>b", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { silent = true })
+map("n", "<leader>fb", ":Telescope buffers<CR>", { silent = true })
 map("n", "<leader>ft", ":TodoTelescope<CR>", { silent = true })
 map("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true })
 map("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true })
